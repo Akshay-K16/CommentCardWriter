3947a880b1404c39ed0a605c1e0b718263f025d6
Akshay Khemaney <khemaney.a@etoncollege.org.uk>
2021-03-12 12:04:33 +0000

    Initial Commit

---
+++ CommentCardWriter/CommentCardWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardWriterApp.swift
+//  CommentCardWriter
+//
+//  Created by Khemaney, Akshay (SPH) on 12/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
